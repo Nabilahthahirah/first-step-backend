a965368c975b83687aa71b8c0a876df2c29567c7
unknown <ahmadsafrizal01@gmail.com>
2023-12-02 16:00:00 +0700

    register & add address

--- src/api/routes/user.route.js
+++ src/api/routes/user.route.js
@@ -1,10 +1,12 @@
 const controllers = require("../controllers/user.controller");
+const { verifyTokenUser } = require("../middlewares/verifyTokenMiddleware");
 
 const router = require("express").Router();
 router.get("/", controllers.getAllUsers);
-// router.get("/:id", controllers.getOneCategory);
-// router.post("/", controllers.newCategory);
-// router.put("/:id", controllers.updateCategory);
-// router.delete("/:id", controllers.deleteCategory);
+router.get("/:id", controllers.getUserId);
+router.post("/register", controllers.registerUser);
+router.post("/login", controllers.loginUser);
+router.put("/", verifyTokenUser, controllers.updateUser);
+router.delete("/", verifyTokenUser, controllers.deleteUser);
 
 module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router()
 const orderRoute= require("./api/routes/order.route")
 const shippingRoute= require("./api/routes/shipping.route")
 const orderStatusRoute= require("./api/routes/order-status.route")
+const userRoute = require("./api/routes/user.route");
 
 router.use("/api/order-status", orderStatusRoute)
 router.use("/api/order", orderRoute)
 router.use("/api/shipping", shippingRoute)
+router.use("/api/user", userRoute);
 
 module.exports = router
