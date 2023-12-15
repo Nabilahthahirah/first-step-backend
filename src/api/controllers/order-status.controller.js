e72f17dc3d74c4d3e35059607119ead02e27e9cf
mahadwiputra008 <mahadwiputra008@gmail.com>
2023-12-15 21:32:52 +0700

    hapus method double

--- src/api/controllers/order-status.controller.js
+++ src/api/controllers/order-status.controller.js
@@ -44,30 +44,6 @@ const getOrderStatusById = async (req, res) => {
   }
 }
 
-
-const getOrderStatusById = async (req, res) => {
-
-  const {id} = req.params
-
-  try {
-
-    const orders = await orderStatusServices.findOrderStatusById(id)
-
-    if (orders.length === 0) {
-      throw new CustomAPIError(`No Order was found`, 400)
-    }
-
-    res.status(200).json({
-      status: "success",
-      message: "Get Order",
-      data: orders,
-    })
-
-  } catch (error) {
-    throw new CustomAPIError(`Error: ${error.message}`, error.statusCode || 500)
-  }
-}
-
 const updateOrderStatus = async (req, res) => {
   try {
 
--- src/app.js
+++ src/app.js
@@ -12,7 +12,11 @@ const app = express();
 /**
  * middleware
  */
-app.use(cors());
+app.use(cors({
+  origin: 'http://admin.first-step.my.id',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
